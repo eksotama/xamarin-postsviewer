d0c032cab248c25463b3024a398f729edab41145
munsif <munsifmusthafa03@gmail.com>
2018-09-14 04:44:17 +0530

    Changes to the list view

---
+++ XFPostsViewer/XFPostsViewer/CommentsListPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XFPostsViewer
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class CommentsListPage : ContentPage
+	{
+		public CommentsListPage ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
--- XFPostsViewer/XFPostsViewer/PostsListPage.xaml.cs
+++ XFPostsViewer/XFPostsViewer/PostsListPage.xaml.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using Xamarin.Forms;
+﻿using Xamarin.Forms;
 using System.Collections.ObjectModel;
 using XFPostsViewer.Data;
 using XFPostsViewer.Service;
